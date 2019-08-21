39150de0e6931e805e48ae7c5043eecb07a84af1
Siva <carah.tyche@gmail.com>
2019-08-20 19:08:00 -0500

    Displayed all resume data on frontend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,71 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <div id="testcase">
-      <section id="content">
-        <h2>{{ student.firstName }} {{ student.lastName }}</h2>
-        <button v-on:click="renderPDF()">Make PDF</button>
-      </section>
+        <div class="resume">
+          <h1>{{ student.firstName }} {{ student.lastName }}</h1>
+          <div class="contact">
+          <div>
+            {{ student.email }}
+          </div>
+          <div>
+            {{ student.phoneNumber }}
+          </div>
+          <div>
+            {{ student.linkedinURL }}
+          </div>
+          <div>
+            {{ student.twitterHandle }}
+          </div>
+          <div>
+            {{ student.blog }}
+          </div>
+          <div>
+            {{ student.resumeURL }}
+          </div>
+          <div>
+            {{ student.gitHub }}
+          </div>
+          <div class="content">
+          <div>
+            {{ student.photo }}
+          </div>
+          <div>
+            {{ student.bio }}
+          </div>
+
+          <h1>Experience:</h1>
+          <div v-for="experience in student.experience">
+            <h2>
+              {{ experience.jobTitle }}
+            </h2>
+            <div>
+              <strong>{{ experience.company }}</strong> {{ experience.startDate }} - {{ experience.endDate }}
+            </div>
+            <div>
+              {{ experience.details }}
+            </div>
+          </div>
+          <h1>Education:</h1>
+          <div v-for="education in student.education">
+            <strong>{{ education.university }}</strong> {{ education.startDate }} - {{ education.endDate }}
+            <div>
+              {{ education.degree }}
+            </div>
+            <div>
+              {{ education.details }}
+            </div>
+          </div>
+          <h1>Skills:</h1>
+          <div v-for="skill in student.skills">
+            {{ skill }} 
+          </div>
+          <h1>Capstone:</h1>
+          <div v-for="capstone in student.capstone">
+            {{ capstone }}
+          </div>
+          </div>
+        </div>
+        </div>
+      <button v-on:click="renderPDF()">Make PDF</button>
     </div>
   </div>
 </template>
@@ -15,13 +75,61 @@
 import * as jsPDF from "jspdf";
 import axios from "axios";
 
+
 export default {
   data: function() {
     return {
-      message: "Welcome to the PDF Renderer!!",
       student: {
-        firstName: "Kathleen",
-        lastName: "Tresslar"
+        firstName: "Peter",
+        lastName: "Jang",
+        email: "peter@email.com",
+        phoneNumber: "123-456-7890",
+        bio: "...",
+        linkedinURL: "...@linkedin.com",
+        twitterHandle: "@PJCoolIce",
+        blog: "...@blog.com",
+        resumeURL: "...@resume.com",
+        gitHub: "...@gitHub.com",
+        photo: "...",
+        experience: [
+          {
+            startDate: "1/2/1802",
+            endDate: "8/19/1901",
+            jobTitle: "Junior Headmaster",
+            company: "Actualize",
+            details: "... .... ..."
+          },
+          {
+            startDate: "1/1/1901",
+            endDate: "1/2/1902",
+            jobTitle: "Headmaster",
+            company: "Hogwarts",
+            details: "... .... ..."
+          },
+          {
+            startDate: "1/2/1902",
+            endDate: "8/19/2019",
+            jobTitle: "Headmaster",
+            company: "Actualize",
+            details: "... .... ..."
+          }
+        ],
+        education: [
+          {
+            startDate: "9/1/1894",
+            endDate: "1/1/1901",
+            degree: "Muggle Studies",
+            university: "Hogwarts",
+            details: "... .... ..."
+          }
+        ],
+        skills: ["Ruby", "HTML", "Potions", "Strong Earth Magnets", "Arithmancy"],
+        capstone: {
+          name: "Magical Applications of Computer Programming",
+          description: "...",
+          url: "capstone@hogwarts.edu",
+          screenshot: "..."
+        }
       }
     };
   },
@@ -38,6 +146,6 @@ export default {
       doc.text(`First name: ${this.student.firstName}`, 20, 20);
       doc.save("resume.pdf");
     }
-  }
+  },
 };
 </script>
