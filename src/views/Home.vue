decb8180c68205bbabc07400d4160c321d9e4003
KathleenTresslar <kathleen.tresslar@gmail.com>
2019-08-20 20:12:04 -0500

    adding index and show actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,57 @@
 <template>
   <div class="home">
+    <div class="index">
+      <div v-for="student in students">
+        <button v-on:click="getInfo(student)">{{student.first_name}}</button>
+      </div>
+    </div>
         <div class="resume">
-          <h1>{{ student.firstName }} {{ student.lastName }}</h1>
+          <h1>{{ student.first_name }} {{ student.last_name }}</h1>
           <div class="contact">
           <div>
             {{ student.email }}
           </div>
           <div>
-            {{ student.phoneNumber }}
+            {{ student.phone_number }}
           </div>
           <div>
-            {{ student.linkedinURL }}
+            {{ student.linkedin_url }}
           </div>
           <div>
-            {{ student.twitterHandle }}
+            {{ student.twitter_handle }}
           </div>
           <div>
             {{ student.blog }}
           </div>
           <div>
-            {{ student.resumeURL }}
+            {{ student.resume_url }}
           </div>
           <div>
-            {{ student.gitHub }}
+            {{ student.github_url }}
           </div>
           <div class="content">
           <div>
-            {{ student.photo }}
+            {{ student.photo_url }}
           </div>
           <div>
-            {{ student.bio }}
+            {{ student.short_bio }}
           </div>
 
           <h1>Experience:</h1>
-          <div v-for="experience in student.experience">
+          <div v-for="experience in student.experiences">
             <h2>
-              {{ experience.jobTitle }}
+              {{ experience.job_title }}
             </h2>
             <div>
-              <strong>{{ experience.company }}</strong> {{ experience.startDate }} - {{ experience.endDate }}
+              <strong>{{ experience.company_name }}</strong> {{ experience.start_date }} - {{ experience.end_date }}
             </div>
             <div>
               {{ experience.details }}
             </div>
           </div>
           <h1>Education:</h1>
-          <div v-for="education in student.education">
-            <strong>{{ education.university }}</strong> {{ education.startDate }} - {{ education.endDate }}
+          <div v-for="education in student.educations">
+            <strong>{{ education.university_name }}</strong> {{ education.start_date }} - {{ education.end_date }}
             <div>
               {{ education.degree }}
             </div>
@@ -56,11 +61,12 @@
           </div>
           <h1>Skills:</h1>
           <div v-for="skill in student.skills">
-            {{ skill }} 
+            {{ skill.skill_name }} 
           </div>
           <h1>Capstone:</h1>
-          <div v-for="capstone in student.capstone">
-            {{ capstone }}
+          <div v-for="capstone in student.capstones">
+            {{ capstone.name }}
+            {{ capstone.description }}
           </div>
           </div>
         </div>
@@ -80,84 +86,38 @@ export default {
   data: function() {
     return {
       message: "welcome to the pdf renderer!",
-      student: {
-        firstName: "Peter",
-        lastName: "Jang",
-        email: "peter@email.com",
-        phoneNumber: "123-456-7890",
-        bio: "...",
-        linkedinURL: "...@linkedin.com",
-        twitterHandle: "@PJCoolIce",
-        blog: "...@blog.com",
-        resumeURL: "...@resume.com",
-        gitHub: "...@gitHub.com",
-        photo: "...",
-        experience: [
-          {
-            startDate: "1/2/1802",
-            endDate: "8/19/1901",
-            jobTitle: "Junior Headmaster",
-            company: "Actualize",
-            details: "... .... ..."
-          },
-          {
-            startDate: "1/1/1901",
-            endDate: "1/2/1902",
-            jobTitle: "Headmaster",
-            company: "Hogwarts",
-            details: "... .... ..."
-          },
-          {
-            startDate: "1/2/1902",
-            endDate: "8/19/2019",
-            jobTitle: "Headmaster",
-            company: "Actualize",
-            details: "... .... ..."
-          }
-        ],
-        education: [
-          {
-            startDate: "9/1/1894",
-            endDate: "1/1/1901",
-            degree: "Muggle Studies",
-            university: "Hogwarts",
-            details: "... .... ..."
-          }
-        ],
-        skills: ["Ruby", "HTML", "Potions", "Strong Earth Magnets", "Arithmancy"],
-        capstone: {
-          name: "Magical Applications of Computer Programming",
-          description: "...",
-          url: "capstone@hogwarts.edu",
-          screenshot: "..."
-        }
-      }
+      student: {},
+      students: {}
     };
   },
   created: function() {
-    // axios.get("/api/students/" + this.$route.params.id).then(response => {
-    //   this.student = response.data;
-    //   console.log(this.student);
-    // });
+    axios.get("https://sleepy-citadel-35395.herokuapp.com/api/students" ).then(response => {
+      this.students = response.data;
+      console.log(this.students);
+    });
   },
   methods: {
+    getInfo: function(student) {
+      axios.get("https://sleepy-citadel-35395.herokuapp.com/api/students/" + student.id ).then(response => {
+      this.student = response.data;
+      console.log(this.student);
+      });},
+
     renderPDF: function() {
       var doc = new jsPDF();
-      doc.addFont("ArialMS", "Arial", "normal");
-      doc.setFont("Arial");
       doc.setFontSize(14);
 
-      doc.fromHTML(`<b>Full name:</b> ${this.student.firstName} ${this.student.lastName}`, 10, 10);
-      doc.text(`Contact info: ${this.student.email} ${this.student.phoneNumber}`, 10, 20);
-      doc.text(`Social: ${this.student.twitterHandle} ${this.student.linkedinURL} ${this.student.blog}`, 10, 30);
-      doc.text(`Bio: ${this.student.bio}`, 10, 40);
+      doc.text(`Full name: ${this.student.first_name} ${this.student.last_name}`, 10, 10);
+      doc.text(`Contact info: ${this.student.email} ${this.student.phone_number}`, 10, 20);
+      doc.text(`Social: ${this.student.twitter_handle} ${this.student.linkedin_url} ${this.student.personal_url}`, 10, 30);
+      doc.text(`Bio: ${this.student.short_bio}`, 10, 40);
       doc.line(10, 50, 200, 50, "s");
       doc.text(`Experience: `, 10, 60);
-      var experiences = this.student.experience;
+      var experiences = this.student.experiences;
       var x = 10;
       var y = 70;
       experiences.forEach(function(experience) {
-        doc.text(`${experience.jobTitle}, ${experience.company}, ${experience.startDate}- ${experience.endDate}`, x, y);
+        doc.text(`${experience.job_title}, ${experience.company_name}, ${experience.start_date}- ${experience.end_date}`, x, y);
         y += 10;
       });
 
@@ -167,9 +127,9 @@ export default {
 
       doc.text(`Education:`, x, y);
       y += 10;
-      var education = this.student.education;
+      var education = this.student.educations;
       education.forEach(function(school) {
-        doc.text(`${school.university}, ${school.startDate} - ${school.endDate}, degree in ${school.degree}`, x, y);
+        doc.text(`${school.university_name}, ${school.start_date} - ${school.end_date}, degree in ${school.degree}`, x, y);
         y += 10;
       });
       y += 10;
@@ -177,11 +137,13 @@ export default {
       doc.line(10, y, 200, y, "s");
       y += 10;
 
-      doc.text(`Capstone project: ${this.student.capstone.name}`, 10, y);
+      doc.text(`Capstone projects:`, 10, y)
       y += 10;
-      doc.text(`${this.student.capstone.description}`, 10, y);
-      y += 10;
-      doc.text(`${this.student.capstone.url}`, 10, y);
+      var capstone = this.student.capstones;
+      capstone.forEach(function(capstone){
+        doc.text(`${capstone.name}: ${capstone.description}`, x, y)
+        y +=10;
+      });
       y += 10;
       doc.line(10, y, 200, y, "s");
       y += 10;
@@ -189,7 +151,7 @@ export default {
       y += 10;
       var skills = this.student.skills;
       skills.forEach(function(skill) {
-        doc.text(`${skill}`, 10, y);
+        doc.text(`${skill.skill_name}`, 10, y);
         y += 10;
       });
 
